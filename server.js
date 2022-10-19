efc6812ef169e77aeb83c4061c4531757b7c1a05
ebor94 <bortega@CeramicaItalia.com>
2022-10-19 09:16:26 -0500

    UI

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import spaceRoute from './routes/SpaceRoute'
 
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//  headers and cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 app.get("/space/", spaceRoute);
 
 app.listen(port, () => {
